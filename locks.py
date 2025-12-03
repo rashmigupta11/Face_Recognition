c4cf4bcd44f86499480665972416ad1ae3356524
Rashmi Gupta <134140721+rashmigupta11@users.noreply.github.com>
2025-12-03 15:23:57 +0530

    Delete locks.py

--- locks.py
+++
@@ -1,6 +0,0 @@
-'''This module contain locks that will be shared among multiple modules'''
-
-import threading
-
-# Create a lock to prevent multiple threads from accessing the buffer at the same time
-lock = threading.Lock()
